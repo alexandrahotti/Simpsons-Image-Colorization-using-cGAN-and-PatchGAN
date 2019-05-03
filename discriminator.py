463a53fbec00e55ddf4fa0297aa7868a1cdf4ea3
jacobmalmberg <jacobmalmberg@gmail.com>
2019-05-03 16:55:55 +0200

    network v9
    extension of network 2 with patchGAN mean to min, lr_scheduler, correct plots, normalization of img, sgd + adam

--- discriminator.py
+++ discriminator.py
@@ -1,5 +1,10 @@
-import torch.nn.functional as F
+import torch
+#import torch.nn.functional as F
 import torch.nn as nn
+
+import matplotlib.pyplot as plt
+from scipy.misc import imread
+import numpy as np
 
 class Discriminator(nn.Module):
     """ The GAN generator that uses a encoder decoder architecture
@@ -8,38 +13,43 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
-
-        # Encoding layers
+        #här
         self.conv_1 = nn.Sequential(
-        nn.Conv2d(3, 64, 3, stride=2, padding=1, bias=False), # in channel, out channel, filter kernel size
-        nn.BatchNorm2d( 64 ),
-        nn.LeakyReLU( 0.1 )
+        nn.Conv2d(3, 64, 6, stride=2, padding=2, bias=False), # in channel, out channel, filter kernel size
+        # nn.BatchNorm2d( 64 ),
+        nn.LeakyReLU( 0.2 )
         )
 
         self.conv_2 = nn.Sequential(
-        nn.Conv2d(64, 128, 3, stride=2, padding=1, bias=False), # in channel, out channel, filter kernel size
+        nn.Conv2d(64, 128, 6, stride=2, padding=2, bias=False), # in channel, out channel, filter kernel size
         nn.BatchNorm2d( 128 ),
-        nn.LeakyReLU( 0.1 )
+        nn.LeakyReLU( 0.2 )
         )
 
         self.conv_3 = nn.Sequential(
-        nn.Conv2d(128, 256, 3, stride=2, padding=1, bias=False), # in channel, out channel, filter kernel size
+        nn.Conv2d(128, 256, 4, stride=2, padding=1, bias=False), # in channel, out channel, filter kernel size
         nn.BatchNorm2d( 256 ),
-        nn.LeakyReLU( 0.1 )
+        nn.LeakyReLU( 0.2 )
         )
 
         self.conv_4 = nn.Sequential(
-        nn.Conv2d(256, 512, 3, stride=2, padding=1, bias=False), # in channel, out channel, filter kernel size
+        nn.Conv2d(256, 512, 3, stride=1, padding=1, bias=False), # in channel, out channel, filter kernel size
         nn.BatchNorm2d( 512 ),
-        nn.LeakyReLU( 0.1 )
+        nn.LeakyReLU( 0.2 )
         )
 
         self.conv_5 = nn.Sequential(
-        nn.Conv2d(512, 1, 3, stride=2, padding=1, bias=False), # in channel, out channel, filter kernel size
+        nn.Conv2d(512, 1, 3, stride=1, padding=1, bias=False), # in channel, out channel, filter kernel size
+
         nn.Sigmoid()
         )
 
+
+
+
+
     def forward(self, data):
+        #
         # print('data.size()')
         # print(data.size())
         output1 = self.conv_1(data)
@@ -58,8 +68,30 @@ class Discriminator(nn.Module):
         output5 = self.conv_5(output4)
         # print(output5.size())
 
+        # output6 = self.conv_6(output5)
+        # print(output6.size())
+        #
+        # output7 = self.conv_7(output6)
+        # print(output7.size())
+        #
+        # output8 = self.conv_8(output7)
+        # print(output8.size())
+
         # Decoding
 
         # print(output5.size())
 
         return output5
+
+if __name__ == "__main__":
+    discriminator = Discriminator()
+    #gan = GeneratorWithSkipConnections()
+    #hl_graph = hl.build_graph(model, torch.zeros([1, 3, 224, 224]))
+    #hl_graph = hl.build_graph(gan, torch.zeros([1, 1, 256, 256]))
+
+    image_array = imread('TheSimpsonsS10E01LardoftheDance.mp40060.jpg')
+    image_array = np.matrix.transpose(image_array)
+    image_array = torch.tensor([image_array]) # nu int 32
+    image_array = image_array.type('torch.FloatTensor')
+    #generated_im = gan.forward(image_array)
+    output = discriminator(image_array)
